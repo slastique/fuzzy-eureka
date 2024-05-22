30b1a3669794567ed520d19c46966a068b3f9f7a
slastique <ruslan.mamaev@gmail.com>
2024-05-22 08:23:10 +0300

    less37

--- cards/deck.go
+++ cards/deck.go
@@ -40,3 +40,14 @@ func (d deck) toString() string {
 func (d deck) saveToFile(filename string) error {
 	return os.WriteFile(filename, []byte(d.toString()), 0755)
 }
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+
+	return deck(s)
+}
